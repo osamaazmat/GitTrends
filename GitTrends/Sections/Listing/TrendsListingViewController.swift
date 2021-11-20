f9005dd8d5db887c4219e27de70fb9ff15cca747
Osama Azmat Khan <usama.azmat@hotmail.com>
2021-11-20 15:27:32 +0500

    Initial commit - Project basic setup complete

---
+++ GitTrends/Sections/Listing/TrendsListingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrendsListingViewController.swift
+//  GitTrends
+//
+//  Created by Osama Azmat Khan on 20/11/2021.
+//
+
+import UIKit
+
+class TrendsListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
